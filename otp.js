aab270b5f54b19458c8929e2aa0333e1e677f6d6
ShinyKirubaSeeli <shinye@unitedtechno.com>
2026-06-02 15:17:26 +0530

    Update otp.js

--- otp.js
+++ otp.js
@@ -1,14 +1,6 @@
 document.querySelector("#submit").addEventListener("click",placeOrder);
 document.querySelector("input+p").addEventListener("click",resendOTP);
 let cartArr = JSON.parse(localStorage.getItem('cartData')) || []
-function resendOTP(){
-    // window.addEventListener("load",function(){
-    //     console.log(document.querySelector("#otP").value)
-       
-        window.location.reload();
-        alert("One-time Password(OTP) is Sending to your registered mobile number!")
-//  });
-}
 function placeOrder(){
     let Otp=document.querySelector("#otP").value;
     // if(Otp==null){
